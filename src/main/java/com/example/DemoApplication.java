a0227d17b32bf7520c57e3187fb71b4c05f1b2e6
lloveys <lyndon.loveys@keyin.com>
2021-11-28 15:00:39 -0330

    All classes, repos and controllers are working. Still have relational issues @ManytoOne

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import com.example.accessingdatarest.Member;
-//import com.example.accessingdatarest.MemberRepository;
+//import com.example.Repositories.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
